45bdabee06426bcc5f597f075fc3dc757e08d17f
track217 <412641046@qq.com>
2017-06-28 17:25:02 +0800

    Update 182. Duplicate Emails.sql

--- 182. Duplicate Emails.sql
+++ 182. Duplicate Emails.sql
@@ -8,17 +8,17 @@
 | 3  | a@b.com |
 +----+---------+
 
-解法一：使用WHERE,757ms
+解法一：将两张表进行比较，ID不等而邮箱相等的数据输出，757ms
 SELECT DISTINCT P1.Email
 FROM Person AS P1, Person AS P2
 WHERE P1.Id != P2.Id AND P1.Email = P2.Email
 
-解法二：使用GROUP BY和HAVING COUNT ,筛选分组，712ms
+解法二：使用 GROUP BY和 HAVING COUNT ,先筛选分组再计数，712ms
 SELECT Email 
 FROM Person
 GROUP BY Email
 HAVING COUNT(*)>1
 
-解法三：使用JOIN联结，773ms
+解法三：使用 JOIN 联结，773ms
 SELECT DISTINCT P1.Email
 FROM Person AS P1 JOIN Person AS P2 ON (P1.Id != P2.Id) AND (P1.Email = P2.Email);
