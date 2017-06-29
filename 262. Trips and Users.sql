e2907e882c7c8356d3db8ab8341ac2173ba0086d
track217 <412641046@qq.com>
2017-06-29 16:29:17 +0800

    Create 262. Trips and Users.sql

---
+++ 262. Trips and Users.sql
@@ -0,0 +1,43 @@
+262. Trips and Users
+表Trips
++----+-----------+-----------+---------+--------------------+----------+
+| Id | Client_Id | Driver_Id | City_Id |        Status      |Request_at|
++----+-----------+-----------+---------+--------------------+----------+
+| 1  |     1     |    10     |    1    |     completed      |2013-10-01|
+| 2  |     2     |    11     |    1    | cancelled_by_driver|2013-10-01|
+| 3  |     3     |    12     |    6    |     completed      |2013-10-01|
+| 4  |     4     |    13     |    6    | cancelled_by_client|2013-10-01|
+| 5  |     1     |    10     |    1    |     completed      |2013-10-02|
+| 6  |     2     |    11     |    6    |     completed      |2013-10-02|
+| 7  |     3     |    12     |    6    |     completed      |2013-10-02|
+| 8  |     2     |    12     |    12   |     completed      |2013-10-03|
+| 9  |     3     |    10     |    12   |     completed      |2013-10-03| 
+| 10 |     4     |    13     |    12   | cancelled_by_driver|2013-10-03|
++----+-----------+-----------+---------+--------------------+----------+
+表Users
++----------+--------+--------+
+| Users_Id | Banned |  Role  |
++----------+--------+--------+
+|    1     |   No   | client |
+|    2     |   Yes  | client |
+|    3     |   No   | client |
+|    4     |   No   | client |
+|    10    |   No   | driver |
+|    11    |   No   | driver |
+|    12    |   No   | driver |
+|    13    |   No   | driver |
++----------+--------+--------+
+返回  撤销率 保留两位小数
++------------+-------------------+
+|     Day    | Cancellation Rate |
++------------+-------------------+
+| 2013-10-01 |       0.33        |
+| 2013-10-02 |       0.00        |
+| 2013-10-03 |       0.50        |
++------------+-------------------+
+
+解法一：使用 CASE统计满足条件个数，使用 ROUND函数保留2位小数，538ms
+SELECT T.Request_at AS Day, ROUND(SUM(CASE WHEN T.Status LIKE 'cancelled_%' THEN 1 ELSE 0 END)/COUNT(*), 2) AS 'Cancellation Rate'
+FROM Trips AS T INNER JOIN Users AS U ON T.Client_Id = U.Users_Id AND U.Banned='No'
+WHERE T.Request_at BETWEEN '2013-10-01' AND '2013-10-03'
+
