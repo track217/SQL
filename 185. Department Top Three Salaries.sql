c3a7dc4249c051f68f783b40abdd1f7925bc6042
track217 <412641046@qq.com>
2017-09-28 11:15:08 +0800

    Update 185. Department Top Three Salaries.sql

--- 185. Department Top Three Salaries.sql
+++ 185. Department Top Three Salaries.sql
@@ -31,7 +31,7 @@
 
 SELECT D.Name AS Department, E.Name AS Employee, E.Salary AS Salary
 FROM Depariment AS D, Employee AS E
-WHERE D.Id = E.DepartmentId AND (SELECT COUNT(Salary)
+WHERE D.Id = E.DepartmentId AND (SELECT COUNT(DISTINCT Salary)
                                  FROM Employee
                                  WHERE DepartmentId = D.Id AND Salary > E.Salary)<3
 ORDER BY D.Name, E.Salary DESC;                                 
