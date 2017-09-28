eeef78d5b244b9740d5ab344733760ed04353b5f
track217 <412641046@qq.com>
2017-09-28 09:31:07 +0800

    Create 626. Exchange Seats.sql

---
+++ 626. Exchange Seats.sql
@@ -0,0 +1,32 @@
+Mary is a teacher in a middle school and she has a table seat storing students' names and their corresponding seat ids.
+
+The column id is continuous increment.
+Mary wants to change seats for the adjacent students.
+Can you write a SQL query to output the result for Mary?
++---------+---------+
+|    id   | student |
++---------+---------+
+|    1    | Abbot   |
+|    2    | Doris   |
+|    3    | Emerson |
+|    4    | Green   |
+|    5    | Jeames  |
++---------+---------+
+
++---------+---------+
+|    id   | student |
++---------+---------+
+|    1    | Doris   |
+|    2    | Abbot   |
+|    3    | Green   |
+|    4    | Emerson |
+|    5    | Jeames  |
++---------+---------+
+
+SELECT IF (
+    id%2 <> 0,
+    if (id <> (SELECT MAX(id) FROM seat), id + 1, id),
+    id - 1
+    ) AS id, student
+FROM seat
+ORDER BY id;
